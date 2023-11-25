1e069c6dcf7390a8e37d111fb931d7a40622a31b
Troff <troffibp@gmail.com>
2023-11-26 00:07:18 +0300

    initial

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector(".icon-menu").addEventListener("click", () => {
+  document.querySelector(".icon-menu").classList.toggle("_active");
+  document.querySelector(".menu__body").classList.toggle("_active");
+});
+
+(function () {
+  const blocks = document.querySelectorAll('fullsize');
+  
+})();
